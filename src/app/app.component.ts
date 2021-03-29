be986d08e036d143ab3debb68419eb78a5987f44
Ruben Mertens <=>
2021-03-29 06:39:35 +0200

    basics done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'expsenses-app';
+  expenses = [
+    {
+      description: 'new Iphone',
+      amount: 1010,
+      date: new Date(2020, 1, 1),
+      deleted: false
+    },
+    {
+      description: 'Hotel bill',
+      amount: 85,
+      date: new Date(2020, 3, 12),
+      deleted: false
+    },
+    {
+      description: 'Coffee',
+      amount: 2.50,
+      date: new Date(2021, 4, 9),
+      deleted: false
+    }
+  ];
+
+  deleteExpense(e: any): void {
+    e.deleted = true;
+  }
 }
